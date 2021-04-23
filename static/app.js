84d0e84edc7216d3166d8b98b597e210783464c7
Daniel Knapp <knapparatus@gmail.com>
2021-04-23 14:04:19 -0500

    Still troubleshooting axios response errors, something to do with promises and chart rendering

--- static/app.js
+++ static/app.js
@@ -1,33 +1,77 @@
 // set base URL for application
 const BASE_URL = 'http://127.0.0.1:5000/api';
 
-let budgetDataResp = [];
+// let budgetDataResp;
+
+// let budgetData = [];
 let generalFundLabel = [];
 let generalFundData = [];
+let revenuesLabel = [];
+let revenuesData = [];
 
-/** processGetRequest: get data based on year making AJAX call to our API. */
+// const budgetDataResp = function(data) {
+// 	// name = data.name;
+// 	console.log(data);
+
+// 	return data;
+// };
+
+/** Get data from API */
 
 async function processGetRequest() {
-	const resp = await axios.get(`${BASE_URL}/2020`);
-
-	console.log(resp.data);
-	return resp.data;
+	const response = await axios(`${BASE_URL}/2020`)
+		.then(response => {
+			const data = response.data;
+			console.log(data);
+			return data;
+		})
+		.catch(err => {
+			console.log('Error: ', err);
+		});
+	console.log(response);
+	return response;
 }
 
-budgetDataResp = await processGetRequest();
+let budgetData = processGetRequest();
 
-// let budgetDataEntries = Object.entries(budgetDataResp[0]);
+// let budgetData = budgetDataResp(
+// 	axios
+// 		.get(`${BASE_URL}/2020`)
+// 		.then(response => {
+// 			const data = response.data;
+// 			console.log(data);
+// 			return data;
+// 		})
+// 		.then(budgetDataResp)
+// 		.catch(err => {
+// 			console.log('Error: ', err);
+// 		})
+// );
 
-console.log(budgetDataResp.general_fund);
+console.log(budgetData);
+console.log(budgetData.budgetData[0]);
 
+/** Get General Fund Data and Labels for chart  **/
 
-
-// push general fund data into chart data and label arrays
-for (const [budgetType, budgetData] of Object.entries(budgetDataResp)) {
-
-	// generalFundData.push(deptFund.allocation);
-	// generalFundLabel.push(deptFund.dept);
+const budgetDataGeneralFund = budgetData.budgetData[0].general_fund;
+console.log(budgetDataGeneralFund);
+for (const lineItem in budgetDataGeneralFund) {
+	// console.log(budgetDataGeneralFund[lineItem].budget);
+	generalFundData.push(budgetDataGeneralFund[lineItem].budget);
+	generalFundLabel.push(budgetDataGeneralFund[lineItem].dept);
 }
+
+/** Get Revenue Data and Labels for chart  **/
+
+const budgetDataRevenues = budgetData.budgetData[1].revenues;
+console.log(budgetDataRevenues);
+for (const lineItem in budgetDataRevenues) {
+	// console.log(budgetDataObj[lineItem].budget);
+	revenuesData.push(budgetDataRevenues[lineItem].budget);
+	revenuesLabel.push(budgetDataRevenues[lineItem].dept);
+}
+
+/** Create and render General Fund chart  **/
 
 var ctxGeneralFund = document.getElementById('generalFundChart').getContext('2d');
 var generalFundChart = new Chart(ctxGeneralFund, {
@@ -82,17 +126,19 @@ var generalFundChart = new Chart(ctxGeneralFund, {
 	}
 });
 
+/** Create and render Revenues chart  **/
+
 // var ctxRevenues = document.getElementById('revenuesChart').getContext('2d');
 // var revenuesChart = new Chart(ctxRevenues, {
 // 	type: 'doughnut',
 // 	data: {
 // 		// labels: [ 'Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange' ],
-// 		labels: generalFundLabel,
+// 		labels: revenuesLabel,
 // 		datasets: [
 // 			{
 // 				label: '2020 General Fund Appropriations',
 // 				// data: [ 12, 19, 3, 5, 2, 3 ],
-// 				data: generalFundData,
+// 				data: revenuesData,
 // 				backgroundColor: [
 // 					'rgba(255, 99, 132, 0.2)',
 // 					'rgba(54, 162, 235, 0.2)',
